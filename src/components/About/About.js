2641405ca8761c772572bfce2d31936fc0e660d2
ruchi-04 <78631524+ruchi-04@users.noreply.github.com>
2023-12-28 00:36:45 +0530

    About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/5483063_2824139.jpg";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+            Know Who <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
@@ -52,3 +52,4 @@ function About() {
 }
 
 export default About;
+
