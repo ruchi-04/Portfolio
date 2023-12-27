645544212338fd7d19f84a29924ad2ac8450bfaa
ruchi-04 <78631524+ruchi-04@users.noreply.github.com>
2023-12-28 00:44:09 +0530

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import medical from "../../Assets/Projects/brain.gif";
+import text from "../../Assets/Projects/giphy.gif";
+import churn from "../../Assets/Projects/churn_c.gif";
+import Multiple from "../../Assets/Projects/disease.gif";
+//import suicide from "../../Assets/Projects/suicide.png";
+//import leaf from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -23,68 +23,43 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Multiple}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Disease-Prediction"
+              description="Multiple Disease Prediction Project is done by using various Machine learning Models like Logistic Regression and Support vector machine.This is used for Predicting disease like diabetes and heart using  various features."
+              ghLink="https://github.com/ruchi-04/Multiple-Disease-Prediction"
+              demoLink="https://multiple-disease-prediction-by-ruchi.streamlit.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={medical}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Brain Segmentation"
+              description="using opencv library in MRI images this Project Predicts that whether person have tumor or not.I have used Support vector Machine and it gave us 97.5% Accuracy"
+              ghLink="https://github.com/ruchi-04/Brain_Segmentation"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={text}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Text Generation"
+              description="used the stroies dataset from kaggle and using nltk library, Markov chain model I created this project to create small sentence using different tokenized words.Which helps to create small stories and novels on the basis of frequency of different words."
+              //ghLink="https://github.com/soumyajit4419/Editor.io"             
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={churn}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Customer_churn-Predition"
+              description="A churn ML model is a machine learning model that predicts customer churn, which is the loss of customers or subscribers. The model uses supervised learning methods to analyze various attributes of customers, such as their lifetime, activity, consumption, billing, and service calls3. The model aims to identify the customers who are most likely to churn and help businesses to retain them with effective strategies"
+              ghLink="https://github.com/ruchi-04/customer_churn_prediction"
+              //demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
         </Row>
